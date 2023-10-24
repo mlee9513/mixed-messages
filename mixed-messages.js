5d05b5a006a2b0cc4464414628bade13917b1149
Michael Lee <mlee9513@gmail.com>
2023-10-23 21:35:17 -0700

    added word banks

---
+++ mixed-messages.js
@@ -0,0 +1,4 @@
+const verbs = ['Run', 'Eat', 'Jump', 'write', 'sing']
+const adjevtives = ['Beautiful', 'Brave', 'Curious', 'Energetic', 'Mysterious']
+const nouns = ['Ghost', 'Bear', 'Whale', 'Paris', 'Portland']
+
